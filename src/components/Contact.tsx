25c000b32077d4b91f17487d83ebc3040b5096ad
juanb86 <juan.e.begnis@gmail.com>
2023-08-24 20:41:00 -0300

    feat: add google maps iframe

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,7 +12,7 @@ interface CardProps {
 const cardsContent: CardProps[] = [
   {
     title: "Head Office",
-    description: "Publica 1234, Buenos Aires - Argentina",
+    description: "Publica 1234, Nogoyá - Argentina",
     icon: BackpackIcon,
   },
   {
@@ -22,7 +22,7 @@ const cardsContent: CardProps[] = [
   },
   {
     title: "Call us",
-    description: "011-42123456",
+    description: "03435-423456",
     icon: PhoneIcon,
   },
 ];
@@ -65,16 +65,27 @@ export default function Contact() {
       </div>
       <div className="container z-10 -mt-24 flex pb-24">
         <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-5xl lg:grid-cols-[3fr_4fr] lg:px-8">
-          <div className="m-3 flex flex-col gap-6 rounded bg-white p-8">
-            {cardsContent.map((card) => (
-              <div key={card.title} className="flex">
-                <card.icon className="mb-4 mr-4 h-8 w-8 text-primary" />
-                <div>
-                  <h3 className="font-semibold">{card.title}</h3>
-                  <p className="text-muted-foreground">{card.description}</p>
+          <div className="m-3 overflow-hidden rounded bg-white">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26950.49668392553!2d-59.822807042124616!3d-32.39739863877068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b13510c9697b6f%3A0x56df0d13059518c3!2zTm9nb3nDoSwgRW50cmUgUsOtb3M!5e0!3m2!1ses!2sar!4v1692919317754!5m2!1ses!2sar"
+              allowFullScreen={true}
+              aria-hidden="false"
+              tabIndex={0}
+              className="h-56 w-full border-0"
+            />
+            <div className="flex flex-col gap-6 p-8">
+              {cardsContent.map((card) => (
+                <div key={card.title} className="flex">
+                  <card.icon className="mb-4 mr-4 h-8 w-8 text-primary" />
+                  <div>
+                    <h4 className="font-semibold">{card.title}</h4>
+                    <p className="text-sm text-muted-foreground">
+                      {card.description}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
           <div className="m-3 flex flex-col items-start rounded bg-white p-8">
             <h2 className="mb-4">Send us a message</h2>
